2b8de1c25304847558c43e3dc10433cb44365563
Anandprakashh <anandprakash4488@gmail.com>
2026-05-21 21:20:20 +0100

    feat(terraform): provision EKS platform foundation with screenshots

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -21,3 +21,27 @@ output "repository_url" {
 output "repository_arn" {
   value = module.ecr.repository_arn
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_arn" {
+  value = module.eks.cluster_arn
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_version" {
+  value = module.eks.cluster_version
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "node_group_name" {
+  value = module.eks.node_group_name
+}
